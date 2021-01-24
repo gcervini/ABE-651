9c98af5edabfeccf86a0018b7e3f07f823de9bf7
gcervini <77764185+gcervini@users.noreply.github.com>
2021-01-24 13:38:53 -0500

    Add files via upload
    Exercises uploaded

---
+++ Exercise_3_2_do_four.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+"""
+Gaia Cervini
+Exercise 3.3
+Jan 24 2021
+"""
+
+def do_twice(func, arg):
+    func(arg)
+    func(arg)
+
+
+def print_twice(arg,arg2=' '):
+    print(arg, end=arg2)
+    print(arg, end=arg2)
+    
+
+def do_four(func, arg=None):
+    do_twice(func, arg)
+    do_twice(func, arg)
+
+    
+def print_mainline():
+    print('+',end=' ')
+    do_twice(print_twice, '-') #-
+    print('+',end=' ')
+    do_twice(print_twice, '-')
+    print('+',end='\n')
+    
+    
+def print_secondline(arg=None):
+    print('|',end=' ')
+    do_twice(print_twice,' ')
+    print('|',end=' ')
+    do_twice(print_twice,' ')
+    print('|',end='\n')
+    
+
+def print_grid():
+    print_mainline()
+    do_four(print_secondline)
+    print_mainline()
+    do_four(print_secondline)
+    print_mainline()
+    
+    
+if __name__ == '__main__':
+
+    print_grid()
+    
+
+    
+
